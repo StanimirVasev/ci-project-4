cdb59755ce2eba5480cef2ef6da4cadbc6d65912
Stanimir Vasev <s.vasev97@gmail.com>
2024-07-01 20:33:41 +0000

    Updated product_detail

--- products/views.py
+++ products/views.py
@@ -70,9 +70,13 @@ def product_detail(request, product_id):
     """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
+    categories = Category.objects.all()
+    subcategories = Subcategory.objects.all()
 
     context = {
         'product': product,
+        'categories': categories,
+        'subcategories': subcategories,
     }
 
     return render(request, 'products/product_detail.html', context)
