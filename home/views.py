736e35f00176b5a87bc092c1d102dcadc12a290e
Stanimir Vasev <s.vasev97@gmail.com>
2024-07-01 19:25:20 +0000

    Fixed homepage nav to include all links and updated css

--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
-# Create your views here.
+from products.models import Category, Subcategory
 
 def index(request):
     """ A view to return the index page """
+    categories = Category.objects.all()
+    subcategories = Subcategory.objects.all()
 
-    return render(request, 'home/index.html')
+    context = {
+        'categories': categories,
+        'subcategories': subcategories,
+    }
+
+    return render(request, 'home/index.html', context)
