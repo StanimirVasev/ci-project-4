e4983f6f098b4a17fa8916c437f612f92cdba793
Stanimir Vasev <s.vasev97@gmail.com>
2024-07-01 20:16:33 +0000

    Added stock status

--- products/models.py
+++ products/models.py
@@ -43,6 +43,7 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    stock_status = models.BooleanField(default=True, verbose_name='In Stock')
 
     def __str__(self):
         return self.name
