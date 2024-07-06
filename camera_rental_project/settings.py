afd6fbdf5daa55f0afceb5336405945744c15e45
Stanimir Vasev <s.vasev97@gmail.com>
2024-07-06 18:47:27 +0000

    Trying to debug email setup

--- camera_rental_project/settings.py
+++ camera_rental_project/settings.py
@@ -217,7 +217,7 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
-ACCOUNT_EMAIL_VERIFICATION = 'none'
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
