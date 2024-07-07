d8ebbaba547d3b2b8c64cc489d1fe33734dd7aca
Stanimir Vasev <s.vasev97@gmail.com>
2024-07-07 06:11:59 +0000

    Blog admin updated

--- blog/admin.py
+++ blog/admin.py
@@ -2,13 +2,18 @@ from django.contrib import admin
 from blog.models import Category, Comment, Post
 
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name',)
+    search_fields = ('name',)
 
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'created_on', 'last_modified')
+    list_filter = ('created_on', 'last_modified')
+    search_fields = ('title', 'body')
 
 class CommentAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('author', 'created_on', 'post')
+    list_filter = ('created_on',)
+    search_fields = ('author', 'body')
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Post, PostAdmin)
