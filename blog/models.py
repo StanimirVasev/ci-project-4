8eeeb61d4dc9158f51ecffc6825d40ed9750da18
Stanimir Vasev <s.vasev97@gmail.com>
2024-07-07 06:47:01 +0000

    Updated models.py

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=30)
+    friendly_name = models.CharField(max_length=30, null=True, blank=True)
 
     class Meta:
         verbose_name_plural = "categories"
@@ -9,8 +10,13 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
+    friendly_title = models.CharField(max_length=255, null=True, blank=True)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
@@ -19,11 +25,19 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def get_friendly_title(self):
+        return self.friendly_title
+
+
 class Comment(models.Model):
     author = models.CharField(max_length=60)
+    friendly_author = models.CharField(max_length=60, null=True, blank=True)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     post = models.ForeignKey("Post", on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.author} on '{self.post}'"
+
+    def get_friendly_author(self):
+        return self.friendly_author
