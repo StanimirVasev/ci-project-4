3bdf2264ace3d6d22ca1db520eaec4cb79b33e53
Stanimir Vasev <s.vasev97@gmail.com>
2024-07-09 20:57:18 +0000

    Added widgets.py for blog app

--- blog/admin.py
+++ blog/admin.py
@@ -1,21 +1,26 @@
 from django.contrib import admin
 from blog.models import BlogCategory, Post
+from blog.forms import PostForm, BlogCategoryForm
 
 class BlogCategoryAdmin(admin.ModelAdmin):
+    form = BlogCategoryForm
     list_display = (
         'name',
         'friendly_name',
     )
     ordering = ('name',)
+    search_fields = ('name', 'friendly_name') 
 
 class PostAdmin(admin.ModelAdmin):
+    form = PostForm
     list_display = (
         'title',
         'friendly_title',
         'created_on',
     )
-    list_filter = ('created_on',)
-    ordering = ('created_on',)
+    list_filter = ('created_on', 'blog_categories') 
+    ordering = ('-created_on',)  
+    search_fields = ('title', 'body')  
 
 admin.site.register(BlogCategory, BlogCategoryAdmin)
 admin.site.register(Post, PostAdmin)
--- blog/forms.py
+++ blog/forms.py
@@ -1,16 +1,21 @@
 from django import forms
 from .models import Post, BlogCategory
+from .widgets import CustomClearableFileInput
 
 class PostForm(forms.ModelForm):
+    # Use CustomClearableFileInput for the image field
+    image = forms.ImageField(
+        widget=CustomClearableFileInput
+    )
+    
     blog_categories = forms.ModelMultipleChoiceField(
         queryset=BlogCategory.objects.all(),
         widget=forms.CheckboxSelectMultiple
     )
-
+    
     class Meta:
         model = Post
         fields = ('title', 'body', 'blog_categories', 'image_url', 'image')
-
 
 class BlogCategoryForm(forms.ModelForm):
     class Meta:
