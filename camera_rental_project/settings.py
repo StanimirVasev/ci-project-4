e75f6d9ae9d3da2c0088136c99ad596a4503d7e4
Stanimir Vasev <s.vasev97@gmail.com>
2024-07-10 20:16:08 +0000

    Fixed checkout_success

--- camera_rental_project/settings.py
+++ camera_rental_project/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = 'DEVELOPMENT' in os.environ
-# DEBUG = True
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-stanimirvase-ciproject4-z5s4hvy3fjm.ws.codeinstitute-ide.net',
