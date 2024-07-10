c291c86dbe5e951efdfce9d9e61c9643d554bc77
Stanimir Vasev <s.vasev97@gmail.com>
2024-07-10 01:09:49 +0000

    Added edit/delete blog post

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('post/<int:pk>/', views.blog_detail, name='blog_detail'),
     path('category/<str:category_name>/', views.blog_category, name='blog_category'),
     path('add/', views.add_blog, name='add_blog'),
+    path('edit/<int:pk>/', views.edit_blog, name='edit_blog'),
+    path('delete/<int:pk>/', views.delete_blog, name='delete_blog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Post, BlogCategory
 from products.models import Category, Subcategory
 from .forms import BlogForm 
@@ -54,12 +58,60 @@ def blog_detail(request, pk):
     return render(request, "blog/blog_detail.html", context)
 
 
+@login_required
 def add_blog(request):
+    """ Add a blog post """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only superusers can add blog posts.')
+        return redirect(reverse('home'))  # Redirect to a relevant page if the user is not authorised
+
     if request.method == 'POST':
         form = BlogForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('blog_list')
+            messages.success(request, 'Successfully added the blog post!')
+            return redirect(reverse('blog_list'))  # Redirect to the blog list page
+        else:
+            messages.error(request, 'Failed to add the blog post. Please ensure the form is valid.')
     else:
         form = BlogForm()
+
     return render(request, 'blog/add_blog.html', {'form': form})
+
+
+@login_required
+def edit_blog(request, pk):
+    """ Edit a blog post """
+    # Fetch the blog post or return a 404 error if not found
+    post = get_object_or_404(Post, pk=pk)
+    
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only superusers can edit blog posts.')
+        return redirect(reverse('home'))  # Redirect to a relevant page if the user is not authorised
+
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated the blog post!')
+            return redirect(reverse('blog_detail', args=[post.pk]))
+        else:
+            messages.error(request, 'Failed to update the blog post. Please ensure the form is valid.')
+    else:
+        form = BlogForm(instance=post)
+        messages.info(request, f'You are editing {post.title}')
+    
+    return render(request, 'blog/edit_blog.html', {'form': form, 'post': post})
+
+
+@login_required
+def delete_blog(request, pk):
+    """ Delete a blog post """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only superusers can do that.')
+        return redirect(reverse('home'))  # Redirect to a relevant page if the user is not authorised
+
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    messages.success(request, 'Blog post deleted!')
+    return redirect(reverse('blog_list'))
